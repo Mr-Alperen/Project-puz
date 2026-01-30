9f811b5d03895ac27017e48519dc3c81251b830b
Alperen ERKAN <233141657+Mr-Alperen@users.noreply.github.com>
2026-01-30 18:56:21 +0300

    Update trash.asm

--- trash.asm
+++ trash.asm
@@ -1,170 +1,1 @@
-; Λόγοι ἀναισθήτως δύναται, ἡ σημασία ἐν κενῷ ἀφανής
-; Σζαρ ὑπάρχει ἀλλ' οὐκ ὑπάρχει
-; Σζαρ οὐ διαλέγεται, μικρὸς ἰδία
 
-mov klĉ, 0x0        ; ἀρχὴ τοῦ ἀγνώστου
-κλĉ: 
-    db 0x0          ; σιγή δεδομένων
-    n0p              ; τὸ τίποτε
-m0x: 
-    dŵ 1             ; ἐλάχιστον σπασμὸν
-    hlt              ; συνείδησις παύει
-; συντακτικὸν? ἄγνωστον καὶ ἀσυνάρτητον
-
-·™i@98Ä“Ã˛ˇóq°#T®@πQ®π≈	@˘3ç@¯	˘ˇS˘¥îπ·Éë‡™BÄR„îS@˘øÎ†"TÙ™™î@π®"5 ’÷QX®¿9@í®¯7»
-ã=@πàR†îµë†˛ˇ5W 5‡™˝{V©ÙOU©ˆWT©¯_S©ˇ√ëˇ#’– P∂ é8‘¶	@˘*ç@¯	˘H@9qÛüì	@˘4ç@¯	˘‰o‡É≠‡É≠‡É≠‡É≠‡+Ä=·Éë.q!T‡™ïî 5ËKAy
-)@qËü‡™ïîÄ4ÄRÛ™y(ÄRpQNqhˇˇTNÒíüöq ’0z∞∏0ã÷ Ä·™ÇÄRÖ‡™·™˝{V©ÙOU©ˆWT©¯_S©ˇ√ëˇ#’– P∂ é8‘Í‰o‡É≠‡É≠‡É≠‡É≠‡É≠‡É≠‡É≠‡É≠‡É≠·Éë‡™hî¿	5·Cë‡™dî@	5Ë£@πÈ@π	k¡TËW@˘È@˘	ÎRˇˇ‡™·™\îMˇˇ‡™·™Xîq¶ˇˇ‡™·™Sî|S5‡™·™Nîq‡™„îÙ™‡™‡îüÎ8ˇˇ‡™İîÙ™‡™ŸîüÎéˇˇ‡™’îÙ™‡™“îüÎÛ∑ü‡™ÕîÙ™‡™ îüÎÛ◊ü‡™≈îÙ™‡™¬îüÎÛ«ü
-‡™ΩîÙ™‡™∫îüÎÛßüÄR‡™˝{V©ÙOU©ˆWT©¯_S©ˇ√ëˇ_÷ Ä·™BÄR Ä·™"ÄRÄRÒî†Óˇ5ËKAy
-@q`TÛî¿4(ÄRnˇˇ Ä·™ÄRÄR„îqaˇˇËKAy
-!@q]ˇˇËKAy
-@qYˇˇËKAy
-	@qUˇˇËKAy
-@qQˇˇËKAy
-@qMˇˇËKAy
-1@qIˇˇËÉ@˘ÒË◊üKˇˇËóB9
-SHˇˇËóB9	SEˇˇËóB9SBˇˇÛ≥@π¬îÛ∑@πªîk5ˇˇËìB9)	ÄR	jËü6ˇˇ(P ’·&p ’Ä“%˛ˇó	Ä“'P ’‡	™ ˛ˇóÛ˘' ’i˝ˇóìî@π…&p ’*& ’Yq@âöÛ˘_˝ˇóëî∞‡HXlÄî∞ÃÏ,ÿË <L\l|å‘ˇˇˇú‘ˇˇˇ¨∏ƒ‘ˇˇˇ‘ˇˇˇ–İ#’ˇ—ÙO©˝{©˝√ëÛ™‰o‡≠‡≠‡≠‡≠‡≠‡≠‡≠‡≠‡≠·Cëzî†5·ë‡™vî¿4ÄR˝{S©ÙOR©ˇëˇ_÷Ëc@˘È@˘	ÎmT ÄR˜ˇˇ´˛ˇTËg@˘È@˘	Î‡◊üÒˇˇ#’ˇC—¯_©ˆW©ÙO©˝{©˝ëÛ™ˇ˘6îπ·#ë‡™BÄRaîË@˘ÎTÙ™,î@π5Ë@˘ë ’÷!X®Úş8@í»¯7»
-ã=@π¿5àRîÄ4˘µëÚˇˇ5‡™˝{D©ÙOC©ˆWB©¯_A©ˇCëˇ_÷Û˘‡ ’‡¸ˇó
-î@π©p ’
- ’Yq@âöÛ˘÷¸ˇó∞1ë0@˘
-◊∞1"ë0@˘
-◊∞1Bë0@˘
-◊∞1bë0@˘
-◊∞1Çë0@˘
-◊∞1¢ë0@˘
-◊∞1¬ë0@˘
-◊∞1‚ë0@˘
-◊∞1ë0@˘
-◊∞1"ë0@˘
-◊∞1Bë0@˘
-◊∞1bë0@˘
-◊∞1Çë0@˘
-◊∞1¢ë0@˘
-◊∞1¬ë0@˘
-◊@(#)PROGRAM:test  PROJECT:shell_cmds-240.100.15.702.1
-33333n@$FreeBSD$-r-w-x-e-f-d-c-b-p	-u-g-k-s-t
--z-n-h-O-G-L-S
-===!=<>-eq-ne	-ge
--gt-le-lt
--nt-ot-ef!-a-o()[]missing ]!unexpected operator%s: %s%sclosing paren expectedargument expected%s: bad number%s: out of range)((Ï.@@Ÿ<@	0@hHÃP	¿	¿	¿	¿	¿	¿	¿	¿	¿		¿
-	¿	¿	¿
-	¿	¿@ Pê@@,:PbtÑí®¥ƒ÷Ï¸
-___error___maskrune_abort_faccessat_getegid_geteuid_isatty_lstat_setlocale_stat_strcmp_strrchr_strtoimax_strtol_verrx__DefaultRuneLocale__mh_execute_headerÏ]¿(∏Ï<¿ƒ
-4P†∞<BEa,5AHS\emtÖçñ°©	
-
-	
-
-__mh_execute_header__DefaultRuneLocale___error___maskrune_abort_faccessat_getegid_geteuid_isatty_lstat_setlocale_stat_strcmp_strrchr_strtoimax_strtol_verrxradr://5614542˙Ş¿Ü$Ë$˙Şƒ§X	É– 
-@com.apple.[yRØáú»„Nõ¥¡gÂSI/Œá<Í¬	)ú%Å&öw≤”È@]ÅæΩ2Ih@£≠àK≠Gùv. ∑À†\jT¸≠¨≤Xo∆Èf¿◊——kOXˇ|¥|zÖĞΩãHâ,ßõDüøi≤d∆èÊ≥Ä3¬∏Fì˚{ö˙a•Æì°Ş3ú“YŞ†¯êËÍ,Œ˛√qD&v'∫`@ˇa?Ì*	–äCRÜŞØÑ9şÀà_3È\
-·ßÒŸ4u\ŒìÅîÄÌ{+G¬E≠¨≤Xo∆Èf¿◊——kOXˇ|¥|zÖĞΩãHâ,ß≠¨≤Xo∆Èf¿◊——kOXˇ|¥|zÖĞΩãHâ,ß≠¨≤Xo∆Èf¿◊——kOXˇ|¥|zÖĞΩãHâ,ß†öB¢Z·Oç ƒi˙ö†LoÅÀÅşæ†`7πî†ĞW˙Ş<˙Ş(com.apple.[˙Şb0Ä	*ÜHÜ˜
-†Ä0Ä10
-	`ÜHe0Ä	*ÜHÜ˜
-†Ç
-â0Ç0Çˆ†!0
-	*ÜHÜ˜
-0b10	UUS10U
-
-Apple Inc.1&0$UApple Certification Authority10U
-Apple Root CA0
-111024173941Z
-261024173941Z010	UUS10U
-
-Apple Inc.1&0$UApple Certification Authority1301U*Apple Code Signing Certification Authority0Ç"0
-	*ÜHÜ˜
-Ç0Ç
-Ç¢®qˇ˘k¯%
-òÅ]kYΩZâ≥ÁÂÓŒ9ò#59Hü!´'ŸÓú d8kßœİP∂ƒ[Ò]õTeÜs/bÂ8Ωdrm|iGaú&„ÎÖ¬,$İj¬?à¸Ã¨)L°IyöuvÂ∏–“	¥LSx˚şv°è§©2 ëß∂˙óΩ9™ÕPà´jÌÎf  K¨;-Ô™Á÷…à-i¬§*a/ğáÛ°!πLü¯˘Ÿ- rÆn˙?à≥Ã£@ÓáQñºœ‰-≠¬ÈYN]e3'™g£ì$á«áyπ]ò≥ÓÕi>åHí$≤m◊0æœÿ‰»ßóÅ–#İ8ô_a˛Ö£Å±0ÅÆ0UˇÜ0U%0
-+0Uˇ0ˇ0Uéi¶ƒwBN•VBúQÜĞ“ è	0U#0Ä+–iGîv	˛Ùkç.@¶˜GM^06U/0-0+†)†'Ü%http://www.apple.com/appleca/root.crl0
-	*ÜHÜ˜
-Çps≠ˆRj∑Ş©*—ùvõ≤iñ«¬√·+–Ô]p¯Ğ<ZHGğ9lÄ sŞb∑: £Z„áĞ%JìõÔsìs]„Ùù~ŞÅˆ©ô¡XnúÙâæœOˇ”¸ÛÙr2± 'Ş®”ôèjÊô˚a…OÚ±g0¿∆
-Ó»DÅ§âªÒyA√ÓÆ»kg|ÃlIÜL””jnú¯\8»0áö0Ğg(∏‚öyĞü•;Qb;"Ò~S|√MİIDÚààŒ:[πùÈ≤∑[Rß∆’Xâr∑ÿÃ≥®Pó?ĞjÆ8ø{y1æcπ˜ÖÖÈmäƒ‚î/◊oR•ô±ıDõpíà&wğ0Ç¥0Çú†dÔÍ˛¬9Ë•0
-	*ÜHÜ˜
-010	UUS10U
-
-Apple Inc.1&0$UApple Certification Authority1301U*Apple Code Signing Certification Authority0
-201029183238Z
-261024173941Z0V10	UUS10U
-
-Apple Inc.10UApple Software10USoftware Signing0Ç"0
-	*ÜHÜ˜
-Ç0Ç
-Çø0∏tòOÆ§în1Tm	—<£∞9©–Ê™`"]”‰Ñ¢@Aêd∆`=ëaĞR®E'û,dÀ‰®$
-R:≥”ÏAx¡û—Êx%cÛş AÊ˛5ÈQ\ãÈ—ùq{—…™KûôĞè¿)∏/êo§ªeèÄj◊=hm‰x¬•/¯2È~…]\å> ◊^ùÜ-˜º§MA§JğkÄEP©LçÍj'Ωªàû<?≤R9ó„∑»ÎTRòä/#VeÂÈ*ŸQ¥#˙ı˜Î†ş’öÚ^Ò_á±Glu±±å—„z¡ÏbAt¶É4Ò˘Ztd9?Ynœ,¿`É)<ÁŞO_
-Ûì9_0ç£Ç[0ÇW0Uˇ00U#0Äéi¶ƒwBN•VBúQÜĞ“ è	0ÅñU Åé0Åã0Åà	*ÜHÜ˜cd0{0y+0mkThis certificate is to be used exclusively for functions internal to Apple Products and/or Apple processes.0U%ˇ0
-+05U.0,0*†(†&Ü$http://crl.apple.com/codesigning.crl0U∆Ì>J˚.Ë?’†≥]ŸÁ≥˜h50UˇÄ0	*ÜHÜ˜cd0
-	*ÜHÜ˜
-Çâù®Ä6?Áf∫sÇ#sõù´ÀZ^¬0Oê%`öI1	R6∏∆nJfW?U¨Ê‰“u˜[œeÉ˚(˘ïiJ…©¬ú˛ã  äô~õbAöü¶á∫Hˆq∞ïå$?>π>¯˛g˛ˆÿù:æê¡–IøõÕ:ÄM},„ç}ƒå†[d∞Œo\5π^OÀÚN◊£Ğ”}¢˚Ä‡_7Ğ$E≥ÜÚ¢ÿÿ·L4îñï»Q°X3`oºΩœåV3uH‰ÊkMòÎT159Hk¿œUÍ®xN¬ÎşXo«Q’ÿUAºiUİ£¥Ùş4£LĞÇôÄBĞ2ã7ª ·ÁİÔ5°rV—0Çª0Ç£†0
-	*ÜHÜ˜
-0b10	UUS10U
-
-Apple Inc.1&0$UApple Certification Authority10U
-Apple Root CA0
-060425214036Z
-350209214036Z0b10	UUS10U
-
-Apple Inc.1&0$UApple Certification Authority10U
-Apple Root CA0Ç"0
-	*ÜHÜ˜
-Ç0Ç
-Ç‰ë©	ëğGPÎÌ^yÑ-Î6¢WLUÏãâŞ˘Kl´"0Ë>¯P	”A®ò˘— fú$k–£ª‰*√ïûz§Gã[‘73ÀƒMŒi—…r]’_õÚ%∫UèM]
-Òd5#KY≥î˜ˆúûœP∫¡XPgè¥ ˜À¨, op∂?0å∑Cœù=Û+I(»˛ŒµπŸ^÷À=µ:≠Ùí±!.t’<
-ğb´£qíGSU¡Ø/A≥¯˚„pÕÊ£LE~LkPñAâƒtbÉAá3äÅ±0XÏZ2åh≥èŞesˇg^eºIÿvü3e°wî…-£Çz0Çv0Uˇ0Uˇ0ˇ0U+–iGîv	˛Ùkç.@¶˜GM^0U#0Ä+–iGîv	˛Ùkç.@¶˜GM^0ÇU Ç0Ç0Ç	*ÜHÜ˜cd0ÅÚ0*+https://www.apple.com/appleca/0Å√+0Å∂Å≥Reliance on this certificate by any party assumes acceptance of the then applicable standard terms and conditions of use, certificate policy and certification practice statements.0
-	*ÜHÜ˜
-Ç\6ôL-x∑ÌåõİÛwõÚv“w0O¡ÖÉÖô=G7Ú©õ@é,‘±êÿæÙsõÓ“dÀyO4ÿ¢>˘xˇk»Ï}9ÉãS ”8ƒ±øöO
-kˇ+¸Yß	|@Vt”∑ã#;G£’o$‚Î—∑pşE·' ÒmxÌÁµ®İ~"5 %’Ÿ÷k‘¢$#˜°¨èsÅ`∆[	/í≤¯DH`8û=&g ä3j˜
-ÇœŞÎ£/˘Sj[d¿c3w˜:,VÎĞ!Ğ∫sOµŸ6¡áUŸßôπ2B˚ÿ’qû~°R∑ΩìB$*«∂Mú^c»KÄP™ä’Ğ‰¸–	7∞uu!1Çè0Çã0Åã010	UUS10U
-
-Apple Inc.1&0$UApple Certification Authority1301U*Apple Code Signing Certification AuthoritydÔÍ˛¬9Ë•0
-	`ÜHe†Ç‘0	*ÜHÜ˜
-	1	*ÜHÜ˜
-0	*ÜHÜ˜
-	1
-231027080016Z0/	*ÜHÜ˜
-	1" 1©√ğEïrΩ∑ç6ô…¡Lˇ0¥Àäş√4∑c
-Ñ¿c0<	*ÜHÜ˜cd	1/0-	`ÜHe 1©√ğEïrΩ∑ç6ô…¡Lˇ0¥Àäş√4∑c
-Ñ¿c0Ç)	*ÜHÜ˜cd	1ÇÇ<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-<dict>
-	<key>cdhashes</key>
-	<array>
-		<data>
-		ATGpw9tFlXK9t402mcnBTP8wtMs=
-		</data>
-	</array>
-</dict>
-</plist>
-0
-	*ÜHÜ˜
-ÇyÂoå'Öaûm√ª∏!öµ¶·£◊…≤sB*ºÅsÙö≈§Ÿ3Ü˜;`4á2ÃE–7TÂ·‰˝7G„6}ªr6ˇA‡Jò¥¸A†≠∂jìã¯*k-;º	≠|µ±GíN◊Úf1;%‘ó˝’ôkú@üŞÀ◊CT9‰œ[Ù≈èóA“œ±˜œƒ”˘ŸÚv5¿{«‘µ)SÇ¸ j8CzÃ1üΩáÓsúNÇãmNd¯ïÊfw~†àÒ]bŸtÆàO∏YûÏ!∆ˇœÉàÃéØ»ÄÄtQ2È;]ˇ˚î÷Vş•ˆ„˝ëT™#ò†˝ö9ñ/0≠lëÈğº—Æm
-
-[signal lost]: NULL
-[signal lost]: NULL
-[signal lost]: NULL
-
-panic(cpu 0 caller 0xffffff80002a3f8a): Kernel trap at 0xffffff8010fa2a, type 14=page fault, registers:
-CR0: 0x80050033, CR2: 0x0000000000000000, CR3: 0x00000000f1a0000, CR4: 0x000006f0
-RAX: 0x0000000000000000, RBX: 0xffffff8012345678, RCX: 0x0000000000000001, RDX: 0xdeadbeefcafebabe
-RSP: 0xffffff81fffa0000, RBP: 0xffffff81fffa0040, RSI: 0x0000000000000000, RDI: 0x0f1a2a3f
-R8:  0x0000000000000000, R9:  0x0f12fae5, R10: 0x0000000000000000
-R11: 0x0000000000000246, R12: 0xffffff8023fa1234, R13: 0x25f2a000, R14: 0x0000000000000000
-R15: 0x1209f28a
-
-panic(cpu 0 caller 0xffffff80002a3f8a): Randomized kernel log dump
-
-0xF12A3B    0x0        0x2A      0x1C3F2A    0xA0F12    0xBEEFCA    0x1234AB    0x0        0xDEADBEEF  0x10FA2A
-0x1F12FAE   0x5FA2EA   0x0       0x0F1A2A3F  0xABCDEF    0x0        0x25F2A000  0x1209F28A  0x0        0xCAFEBABE
-0x0         0x0        0x0       0xDEAD      0x0         0xB16B00B5  0x0         0x2A        0x0        0x0
-0x0F0F0F0F  0x0        0x0       0x0         0xFADEBEEF  0x0        0x12345678  0x0        0x0        0x0
-0x0         0xDEADC0DE 0x0       0x0         0x0         0x0        0x0         0x0        0xBADC0DE   0x0
-0x10FA2A    0x0        0x2A      0x0         0x0         0x0        0xDEAD10CC  0x0        0x0         0x0
-0x1234AB    0x0        0x0       0x0         0x0         0x0        0x0         0x0        0x0         0x0
-0x0         0xCAFEBABE 0x0       0x0         0x0         0x2A       0x0         0x0        0x0         0xDEAD
-0x0         0x0        0xB16B00B5 0x0        0x0         0x0        0x0         0x0        0x0         0x0
-0x0F1A2A3F  0x0        0x0       0x0         0x0         0x0        0x0         0x0        0x0         0x0
-
-
-Backtrace (CPU 0), Frame : Return Address
-0xffffff81fffa0040 : 0xffffff80002a3f8a 
-0xffffff81fffa0080 : 0xffffff8000342abc 
-0xffffff81fffa00c0 : 0xffffff8000345def 
-0xffffff81fffa0100 : 0xffffff800030ff12 
